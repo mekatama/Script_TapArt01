a4ef172caa8ab6bcabccf3010ccc65f704132fab
mekatama <tamatte@hotmail.com>
2019-05-17 19:30:59 +0900

    基本設定

---
+++ GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	public int zombieTap;	//Tap数
+
+	//ゲームステート
+	enum State{
+		GameStart,	//
+		Play,		//
+		Clear,		//不要かも
+	}
+	State state;
+
+	void Start () {
+		GameStart();	//初期ステート		
+	}
+
+	void LateUpdate () {
+		//ステートの制御
+		switch(state){
+			case State.GameStart:
+//				Debug.Log("game start");
+				Play();		//ステート移動		
+				break;
+			//
+			case State.Play:
+//				Debug.Log("play");
+				//finish判定
+//				if(isGameOver){
+//					Clear();	//ステート移動
+//				}
+				break;
+			//
+			case State.Clear:
+//				Debug.Log("clear");
+				break;
+		}
+	}
+
+
+	void Update () {
+		Debug.Log("tap : " + zombieTap);
+		
+	}
+
+
+	void GameStart(){
+		state = State.GameStart;
+	}
+	void Play(){
+		state = State.Play;
+	}
+	void Clear(){
+		state = State.Clear;
+	}
+}
