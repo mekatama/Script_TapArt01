0cdc826b44393c95f86eae8833b643aa6e786dd7
mekatama <tamatte@hotmail.com>
2019-05-31 20:15:34 +0900

    ui tap数の表示

---
+++ UI_TapNum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_TapNum : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text tapNumText;				//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//tap数表示
+		tapNumText.text = "zombie : " + gc.zombieTap.ToString("000000" + "p");
+	}
+}
