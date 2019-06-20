b4f109e1bc64ad07bd756ed9f70758e1c2e32b82
mekatama <tamatte@hotmail.com>
2019-06-20 19:07:36 +0900

    animation処理作り直し

--- TapChara.cs
+++ TapChara.cs
@@ -8,6 +8,7 @@ public class TapChara : MonoBehaviour {
 	GameObject gameController;	//検索したオブジェクト入れる用
 	GameObject zombie;			//tapしたオブジェクト入れる用
 	private bool isTap = false;	//一回だけ処理
+//	private bool isDamage = false;	//一回だけ処理
 	Animator anim;				//Animator入れる用
 	public int zombieHP;		//HP
 	private int zombieHPMax;	//MaxHP一時保存用
@@ -29,48 +30,50 @@ public class TapChara : MonoBehaviour {
 
 			if(Physics.Raycast(ray, out hit)){
 				zombie = hit.collider.gameObject;	//tapしたobject取得
+//				Debug.Log("name : " + zombie.transform.name);
 			}
 
 			//Zombieをtapしたら
 			if(zombie.tag == "Zombie"){
+				//再生中animation取得
+				AnimatorClipInfo clipInfo = anim.GetCurrentAnimatorClipInfo (0)[0];
+//				Debug.Log ("アニメーションクリップ名 : " + clipInfo.clip.name);
+				string anim_name = clipInfo.clip.name;	//再生中animation名を保存
 				//タッチに反応
 				if(isTap == false){
 					//自分がタッチされたか判定
 					if(this.transform.name == zombie.name){
-						int animType = Random.Range(0,2);	//ランダムでanimation決める
-						int criticalType = Random.Range(0,4);	//ランダムでクリティカル攻撃決める
-						//gcって仮の変数にGameControllerのコンポーネントを入れる
-						GameController gc = gameController.GetComponent<GameController>();
-						gc.zombieTap ++;					//tap数加算
-						//ダメージ発生
-						if(criticalType < 1){
-							zombieHP = zombieHP - criticalDamage;	//クリティカルダメージ
-							Debug.Log("クリティカルダメージ");
-						}else{
-							zombieHP --;							//通常ダメージ
-						}
-						Debug.Log("HP:" + zombieHP + " : " +	this.transform.name);
-						if(zombieHP > 0){
-							//animator用flag変更
-							anim.SetBool("isDamage",true);
-						}else if(zombieHP <= 0){
-							//animetion分岐
-							switch(animType){
-								case 0:
-									anim.SetBool("isDown",true);	//animator用flag変更
-									zombieHP = zombieHPMax;			//HP初期値にする
-									break;
-								case 1:
-									anim.SetBool("isDown2",true);	//animator用flag変更
-									zombieHP = zombieHPMax;			//HP初期値にする
-									break;
+						//motion_wait再生中かどうか判定
+						if(anim_name == "motion_wait"){
+							int criticalType = Random.Range(0,4);	//ランダムでクリティカル攻撃決める
+							int animType = Random.Range(0,2);		//ランダムでanimation決める
+							//ダメージ発生
+							if(criticalType < 1){
+								//クリティカルダメージ
+								zombieHP = zombieHP - criticalDamage;	//クリティカルダメージ
+								Debug.Log("クリティカルダメージ");
+							}else{
+								//通常ダメージ
+								zombieHP --;							//通常ダメージ
+							}
+
+							//ダメージ処理
+							if(zombieHP > 0){
+								anim.SetBool("isDamage",true);	//animator用flag変更
+							}else if(zombieHP <= 0){
+								//animetion分岐
+								switch(animType){
+									case 0:
+										anim.SetBool("isDown",true);	//animator用flag変更
+										zombieHP = zombieHPMax;			//HP初期値にする
+										break;
+									case 1:
+										anim.SetBool("isDown2",true);	//animator用flag変更
+										zombieHP = zombieHPMax;			//HP初期値にする
+										break;
+								}
 							}
 						}
-						isTap = true;						//一回だけ処理用
-						//SEをその場で鳴らす
-	//					AudioSource.PlayClipAtPoint( audioClipTap, transform.position);
-						//1秒後に呼び出す
-						Invoke("PositionReset", 2.0f);
 					}
 				}
 			}
@@ -83,5 +86,44 @@ public class TapChara : MonoBehaviour {
 		anim.SetBool("isDown2",false);	//animator用flag変更
 		anim.SetBool("isDamage",false);	//animator用flag変更
 		isTap = false;
+	}
+
+	//animation event
+	void motionStart(){
+		isTap = true;
+//		Debug.Log("isTap motionStart : " + isTap);
+//		Debug.Log("animation start!!");
+	}
+	void motionDamageStart(){
+		isTap = true;
+//		Debug.Log("isTap motionDamageStart : " + isTap);
+	}
+	void motionDown1Start(){
+		isTap = true;
+//		Debug.Log("isTap motionDown1Start : " + isTap);
+	}
+	void motionDown2Start(){
+		isTap = true;
+//		Debug.Log("isTap motionDown2Start : " + isTap);
+	}
+
+	void motionDamageFinish(){
+		anim.SetBool("isDamage",false);	//animator用flag変更
+		isTap = false;
+//		isDamage = false;
+//		Debug.Log("isTap motionDamageFinish : " + isTap);
+	}
+	void motionDown1Finish(){
+		anim.SetBool("isDown",false);	//animator用flag変更
+		isTap = false;
+//		Debug.Log("isTap motionDownFinish : " + isTap);
+	}
+	void motionDown2Finish(){
+		anim.SetBool("isDown2",false);	//animator用flag変更
+		isTap = false;
+//		Debug.Log("isTap motionDown2Finish : " + isTap);
+	}
+	void motionWait(){
+//		Debug.Log("wait : " + isTap);
 	}
 }
