a2270407dc42ef046a89fb7553eb0c99af23ed31
mekatama <tamatte@hotmail.com>
2019-06-25 18:49:11 +0900

    称号表示UI表示

---
+++ UI_BonusText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_BonusText : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text syougouText;			//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//tap数表示
+		syougouText.text = "test : " + gc.syougou;
+	}
+}
