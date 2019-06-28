b96469a3f2720e4abaa5ddb3856856ae43e13f48
mekatama <tamatte@hotmail.com>
2019-06-28 19:00:50 +0900

    kill数UI表示

---
+++ UI_killNum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_killNum : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text killNumText;				//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//tap数表示
+		killNumText.text = "kill : " + gc.zombieKill.ToString("000000");
+	}
+}
